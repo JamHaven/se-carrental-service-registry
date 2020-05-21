9dc6c15917cf021ba42378dfac5786a72e1baf1d
philipp <philipp@Philipps-MacBook.local>
2020-05-21 21:09:38 +0200

    disabled eureka authentication

--- src/main/java/pacApp/pacConfig/WebSecurityConfig.java
+++ src/main/java/pacApp/pacConfig/WebSecurityConfig.java
@@ -2,9 +2,11 @@ package pacApp.pacConfig;
 
 import org.springframework.http.HttpMethod;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @SuppressWarnings("SpringJavaAutowiringInspection")
 @Configuration
@@ -16,13 +18,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         httpSecurity
                 .csrf().disable()
                 .authorizeRequests()
-                //.anyRequest().permitAll();
+                //1
+                .anyRequest().permitAll();
+                //2
+                /*
                 .antMatchers(HttpMethod.GET, "/eureka/**").authenticated() // eureka client
-                .antMatchers(HttpMethod.POST, "/eureka/**").authenticated() // eureka client
-                .antMatchers(HttpMethod.PUT, "/eureka/**").authenticated() // eureka client
-                .antMatchers(HttpMethod.DELETE, "/eureka/**").authenticated() // eureka client
-                .antMatchers(HttpMethod.OPTIONS, "/eureka/**").authenticated() // eureka client
-                .anyRequest().authenticated().and().httpBasic(); // dashboard authorization
+                //.antMatchers(HttpMethod.POST, "/eureka/**").authenticated() // eureka client
+                //.antMatchers(HttpMethod.PUT, "/eureka/**").authenticated() // eureka client
+                //.antMatchers(HttpMethod.DELETE, "/eureka/**").authenticated() // eureka client
+                //.antMatchers(HttpMethod.OPTIONS, "/eureka/**").authenticated() // eureka client
+                .anyRequest().permitAll()
+                .and().httpBasic(); // dashboard authorization
+                */
     }
 
 }
