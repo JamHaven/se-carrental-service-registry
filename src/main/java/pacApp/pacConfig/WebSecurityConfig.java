2af23ad8d62b6e6348a9327809b0d5c59315d597
Eren Erdogan <t.eren.erdogan@hotmail.com>
2020-05-21 16:46:00 +0200

    shut down basic auth

--- src/main/java/pacApp/pacConfig/WebSecurityConfig.java
+++ src/main/java/pacApp/pacConfig/WebSecurityConfig.java
@@ -1,28 +1,31 @@
 package pacApp.pacConfig;
 
-import org.springframework.http.HttpMethod;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+/**
+ * shut donw authentication
+ */
+//import org.springframework.http.HttpMethod;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@SuppressWarnings("SpringJavaAutowiringInspection")
-@Configuration
-@EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//@SuppressWarnings("SpringJavaAutowiringInspection")
+//@Configuration
+//@EnableWebSecurity
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    protected void configure(HttpSecurity httpSecurity) throws Exception {
+//        httpSecurity
+//                .csrf().disable()
+//                .authorizeRequests()
+//                //.anyRequest().permitAll();
+//                .antMatchers(HttpMethod.GET, "/eureka/**").authenticated() // eureka client
+//                .antMatchers(HttpMethod.POST, "/eureka/**").authenticated() // eureka client
+//                .antMatchers(HttpMethod.PUT, "/eureka/**").authenticated() // eureka client
+//                .antMatchers(HttpMethod.DELETE, "/eureka/**").authenticated() // eureka client
+//                .antMatchers(HttpMethod.OPTIONS, "/eureka/**").authenticated() // eureka client
+//                .anyRequest().authenticated().and().httpBasic(); // dashboard authorization
+//    }
 
-    @Override
-    protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
-                .csrf().disable()
-                .authorizeRequests()
-                //.anyRequest().permitAll();
-                .antMatchers(HttpMethod.GET, "/eureka/**").authenticated() // eureka client
-                .antMatchers(HttpMethod.POST, "/eureka/**").authenticated() // eureka client
-                .antMatchers(HttpMethod.PUT, "/eureka/**").authenticated() // eureka client
-                .antMatchers(HttpMethod.DELETE, "/eureka/**").authenticated() // eureka client
-                .antMatchers(HttpMethod.OPTIONS, "/eureka/**").authenticated() // eureka client
-                .anyRequest().authenticated().and().httpBasic(); // dashboard authorization
-    }
-
-}
+//}
